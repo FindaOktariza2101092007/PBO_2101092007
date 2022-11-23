e5205fe49e624055bd1ba3b3ef96a86b2df69507
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2022-11-23 16:57:12 +0700

    Add files via upload

---
+++ src/finda231122/BukuAlamat.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finda231122;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BukuAlamat {
+    private String nama;
+    private String alamat;
+    private String notlp;
+    private String email;
+ 
+    public BukuAlamat(){
+        
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    
+    public String getAlamat(){
+        return alamat;
+    }
+    
+    public void setAlamat(String alamat){
+        this.alamat=alamat;
+    }
+    public String getNotlpn(){
+        return notlp;
+    }
+    
+    public void setNotlp(String notlp){
+        this.notlp=notlp;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public void setNilai(String nama, String alamat, String notlp, String email) {
+        this.nama= nama;
+        this.alamat = alamat;
+        this.notlp = notlp;
+        this.email=email;
+    }
+}
