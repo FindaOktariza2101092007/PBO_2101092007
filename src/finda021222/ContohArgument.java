bce9fd9fc65bfbc5610ac4753f93eb35cbbb431f
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2023-01-10 18:31:52 +0700

    Add files via upload

---
+++ src/finda021222/ContohArgument.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finda021222;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ContohArgument {
+    public static void main( String[] args ){
+
+        try{
+            System.out.println("First Argument="+ args[0]);
+        } catch(Exception ex){
+            System.out.println("Eror Argument "+ex+ "Tidak ditemukkan");
+        }
+        
+        
+        } 
+}
