fbd2c4368125fe62411de295a04bd23c65e96acc
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2023-01-10 17:05:51 +0700

    Add files via upload

---
+++ modul_pbo_teori/modul4/modul4Finda/HelloFinda.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4Finda;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HelloFinda {
+    public static void main(String[] args) { 
+ //menampilkan string”Hello world” pada screen 
+ 
+ System.out.println("Hello"); 
+ System.out.println("World"); 
+ }
+}
