91560c45fd9c6f69b151516cb0e690226d1abfc2
FindaOktariza2101092007 <114201253+FindaOktariza2101092007@users.noreply.github.com>
2023-01-10 17:23:57 +0700

    Add files via upload

---
+++ modul_pbo_teori/modul8/Modul8Finda/ContohStatic.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul8Finda;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ContohStatic {
+    public static int tambah(int x, int y){
+        return(x+y);
+    }
+    
+    public static void main(String[] args){
+        
+        int hasil = ContohStatic.tambah(5,8);
+    } 
+}
